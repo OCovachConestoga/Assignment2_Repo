858aa9fc3606cf20bc93b7a43d607bbab10d3ab0
CovOwe04 <owenacov@gmail.com>
2024-09-19 14:25:59 -0400

    Added PreRelease compiler directives

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -5,10 +5,15 @@
 
 using namespace std;
 
-// create the struct for student data
+#define PRE_RELEASE
+
+// Create the struct for student data
 struct STUDENT_DATA {
 	string firstName;
 	string lastName;
+#ifdef PRE_RELEASE
+	string studentEmail;
+#endif
 };
 
 int main() {
@@ -16,8 +21,19 @@ int main() {
 	// Create the list for the students
 	vector<STUDENT_DATA> studentData;
 
+
+#ifdef PRE_RELEASE
+	cout << "Running code in PreRelease mode" << endl << endl;
+
+	// Open the file of student names with emails
+	ifstream fin("StudentData_Emails.txt");
+#else
+	cout << "Running code in Standard mode" << endl << endl;
+
 	// Open the file of student names
 	ifstream fin("StudentData.txt");
+
+#endif
 
 	// Checks if the file had issues opening
 	if (!fin.is_open()) {
@@ -37,9 +53,16 @@ int main() {
 		// Convert to istringstream to parse data correctly
 		istringstream line(finString);
 
+#ifdef PRE_RELEASE
+		// Read the names from the string stream
+		std::getline(line, tempStudent.firstName, ',');
+		std::getline(line, tempStudent.lastName, ',');
+		std::getline(line, tempStudent.studentEmail);
+#else
 		// Read the names from the string stream
 		std::getline(line, tempStudent.firstName, ',');
 		std::getline(line, tempStudent.lastName);
+#endif
 
 
 		// Add the student to the vector space
@@ -47,12 +70,16 @@ int main() {
 	}
 
 	// Only runs this section in debug mode
-	#ifdef _DEBUG
-	// Loop through the vector space and print the names to show the data has been parsed
+#ifdef _DEBUG
+// Loop through the vector space and print the names to show the data has been parsed
 	for (STUDENT_DATA student : studentData) {
+#ifdef PRE_RELEASE
+		cout << student.firstName << "," << student.lastName << " - " << student.studentEmail << endl;
+#else
 		cout << student.firstName << "," << student.lastName << endl;
+#endif	
 	}
-	#endif
+#endif
 
 
 	return 1;
