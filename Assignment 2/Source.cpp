73bf19c873fb17e1629339b423333c932ec710a7
CovOwe04 <owenacov@gmail.com>
2024-09-19 13:41:23 -0400

    Initial commit, parsing student data from the file

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
+// create the struct for student data
+struct STUDENT_DATA {
+	string firstName;
+	string lastName;
+};
+
 int main() {
+
+	// Create the list for the students
+	vector<STUDENT_DATA> studentData;
+
+	// Open the file of student names
+	ifstream fin("StudentData.txt");
+
+	// Checks if the file had issues opening
+	if (!fin.is_open()) {
+		cout << "ISSUE OPENING FILE" << endl;
+		return 1;
+	}
+
+	// Create a temporary string to use for parsing
+	string finString;
+
+	// Loop through the file and parse the data
+	while (std::getline(fin, finString)) {
+
+		// Create temp variables and objects
+		STUDENT_DATA tempStudent;
+
+		// Convert to istringstream to parse data correctly
+		istringstream line(finString);
+
+		// Read the names from the string stream
+		std::getline(line, tempStudent.firstName, ',');
+		std::getline(line, tempStudent.lastName);
+
+
+		// Add the student to the vector space
+		studentData.push_back(tempStudent);
+	}
+
+	// Loop through the vector space and print the names to show the data has been parsed
+	for (STUDENT_DATA student : studentData) {
+		cout << student.firstName << "," << student.lastName << endl;
+	}
+
+
 	return 1;
 }
